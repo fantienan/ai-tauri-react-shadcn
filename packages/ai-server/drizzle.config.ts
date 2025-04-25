2397b8a7a09173d5f85c6d705339cad27e37b265
fantienan <363982607@qq.com>
2025-04-25 08:07:44 +0800

    feat: drizzle orm error

--- packages/ai-server/drizzle.config.ts
+++ packages/ai-server/drizzle.config.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from 'drizzle-kit'
 import { config } from './src/config/index.ts'
 
-export default defineConfig(config.drizzleKit)
+export default defineConfig({ ...config.drizzleKit, out: './src/database/migrations' })
--- packages/ai-server/src/config/config.default.ts
+++ packages/ai-server/src/config/config.default.ts
@@ -1,24 +1,37 @@
-import '../utils/env.ts'
 import path from 'node:path'
 import url from 'node:url'
+import { configDotenv } from 'dotenv'
+import { expand } from 'dotenv-expand'
+import findConfig from 'find-config'
 import fs from 'fs-extra'
 import type { BizConfig } from './types.ts'
 
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
-
+const dirname = path.dirname(url.fileURLToPath(import.meta.url))
 const { NODE_ENV = 'local' } = process.env
+
+let envPath = findConfig('.env', { dot: true })
+
+if (!envPath) throw new Error('Cannot find .env file')
+
+envPath = path.dirname(envPath)
+expand(
+  configDotenv({
+    path: [path.join(envPath, '.env.local'), path.join(envPath, `.env.${NODE_ENV}`), path.join(envPath, '.env')],
+  }),
+)
 
 const genFile = (name: string) => path.resolve(process.env.BIZ_WORKSPACE, '.logs', NODE_ENV!, `${name}.log`)
 
 const config: BizConfig = {
   isProductionEnvironment: process.env.NODE_ENV === 'production',
   drizzleKit: {
-    schema: path.resolve(__dirname, '../database/schema.ts'),
-    out: path.resolve(__dirname, '../database/migrations'),
+    schema: path.resolve(dirname, '../database/schema.ts'),
+    out: path.resolve(dirname, '../database/migrations'),
     dialect: 'sqlite',
     dbCredentials: {
       url: process.env.SQLITE_DATABASE_URL!,
     },
+    verbose: true,
   },
   sqlite: {
     databaseUrl: process.env.SQLITE_DATABASE_URL!,
